cad73664558b4c6b263a5479f9a36c64ba6f782e
Ivan Shulev <ishulev@melontech.com>
2018-07-17 10:33:06 +0300

    add changeToCurrentPage for specific page pagination clicks

--- frontend/app/order-list/components/list/order-list.component.ts
+++ frontend/app/order-list/components/list/order-list.component.ts
@@ -10,14 +10,17 @@ import { GetOrdersFromServerAction } from '../../store/actions';
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
   styleUrls: ['./order-list.component.scss'],
-  animations: [ slideInDownAnimation ]
+  animations: [slideInDownAnimation]
 })
 export class OrderListComponent implements OnInit, OnDestroy {
   @HostBinding('@routeAnimation') routeAnimation = true;
-  @HostBinding('style.display')   display = 'block';
-  @HostBinding('style.position')  position = 'absolute';
-  constructor(private _router: Router, private _store: Store<fromShared.State>, private _currentRoute: ActivatedRoute) {
-  }
+  @HostBinding('style.display') display = 'block';
+  @HostBinding('style.position') position = 'absolute';
+  constructor(
+    private _router: Router,
+    private _store: Store<fromShared.State>,
+    private _currentRoute: ActivatedRoute
+  ) {}
 
   private _pageSub: any;
   private _loadingSub: any;
@@ -31,14 +34,24 @@ export class OrderListComponent implements OnInit, OnDestroy {
     this._store.dispatch(new GetOrdersFromServerAction(nextPage));
   }
   private setClosestPages(currentPage) {
-    if(currentPage < 4) {
+    if (currentPage < 4) {
       this.closestPages = [1, 2, 3, 4, 5];
-    }
-    else if (currentPage > this.lastPage - 3) {
-      this.closestPages = [this.lastPage - 4, this.lastPage - 3, this.lastPage - 2, this.lastPage - 1, this.lastPage];
-    }
-    else {
-      this.closestPages = [currentPage - 2, currentPage - 1, currentPage, currentPage + 1, currentPage + 2];
+    } else if (currentPage > this.lastPage - 3) {
+      this.closestPages = [
+        this.lastPage - 4,
+        this.lastPage - 3,
+        this.lastPage - 2,
+        this.lastPage - 1,
+        this.lastPage
+      ];
+    } else {
+      this.closestPages = [
+        currentPage - 2,
+        currentPage - 1,
+        currentPage,
+        currentPage + 1,
+        currentPage + 2
+      ];
     }
   }
 
@@ -56,6 +69,10 @@ export class OrderListComponent implements OnInit, OnDestroy {
     this.navigateToPage(this.currentPage + 1);
   }
 
+  public changeToPage(page) {
+    this.navigateToPage(page);
+  }
+
   public changeRouteLastPage() {
     this.navigateToPage(this.lastPage);
   }
@@ -65,14 +82,24 @@ export class OrderListComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this._store.dispatch(new GetOrdersFromServerAction(this._currentRoute.snapshot.children[0].params['number']));
-    this._totalPagesSub = this._store.pipe(select(fromShared.getTotalPages)).subscribe(totalPages => this.lastPage = totalPages);
-    this._pageSub = this._store.pipe(select(fromShared.getPage)).subscribe(page => {
-      this.setClosestPages(page);
-      console.log(this.closestPages);
-      this.currentPage = page;
-    });
-    this._loadingSub = this._store.pipe(select(fromShared.isLoading)).subscribe(isLoading => console.log(isLoading));
+    this._store.dispatch(
+      new GetOrdersFromServerAction(
+        this._currentRoute.snapshot.children[0].params['number']
+      )
+    );
+    this._totalPagesSub = this._store
+      .pipe(select(fromShared.getTotalPages))
+      .subscribe(totalPages => (this.lastPage = totalPages));
+    this._pageSub = this._store
+      .pipe(select(fromShared.getPage))
+      .subscribe(page => {
+        this.setClosestPages(page);
+        console.log(this.closestPages);
+        this.currentPage = page;
+      });
+    this._loadingSub = this._store
+      .pipe(select(fromShared.isLoading))
+      .subscribe(isLoading => console.log(isLoading));
   }
 
   ngOnDestroy() {
