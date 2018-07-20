3c8eb2edb0950a1d489d37e0f4b95b98dd4fd447
Ivan Shulev <ishulev@melontech.com>
2018-07-20 10:08:37 +0300

    use routerActive

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -1,33 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Store, select } from '@ngrx/store';
-import { Subject } from 'rxjs/Subject';
-import { takeUntil } from 'rxjs/operators';
-
-import * as fromShared from './shared/store/reducers';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  constructor(private _store: Store<fromShared.State>) {}
-  private _destroyActions = new Subject<boolean>();
-  public routePath = '';
-
-  ngOnInit() {
-    this._store
-      .pipe(takeUntil(this._destroyActions))
-      .pipe(select(fromShared.getRouterPath))
-      .subscribe(newPath => {
-        if (newPath) {
-          this.routePath = newPath;
-        }
-      });
-  }
-
-  ngOnDestroy() {
-    this._destroyActions.next(true);
-    this._destroyActions.complete();
-  }
-}
+export class AppComponent {}
